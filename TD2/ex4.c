616bc33a6c8d199bf939fa62f9924f9231562242
ath-gsy <107257470+ath-gsy@users.noreply.github.com>
2023-02-14 18:15:20 +0100

    Create ex4.c

---
+++ TD2/ex4.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <x86intrin.h>
+
+void acces_seq(int n)
+{
+    int *TAB;
+    TAB = malloc(sizeof(int) * 1000000000);
+    for (int i = 0; i < n; i++)
+    {
+        TAB[i] = i;
+    }
+}
+
+void acces_alea(int n)
+{
+    int *TAB;
+    TAB = malloc(sizeof(int) * 1000000000);
+    for (int i = 0; i < n; i++)
+    {
+        TAB[rand()] = i;
+    }
+}
+
+void print_timing(int mon_arg, void (*ma_fonction)(int), int nombre_init, int nombre_iter)
+{
+
+    // init
+    unsigned int ui;
+    for (int i = 0; i < nombre_init; i++)
+    {
+        __rdtscp(&ui);
+    }
+    ////
+
+    // calcul
+
+    unsigned long int *tab;
+    tab = malloc(nombre_iter * sizeof(unsigned long int));
+
+    unsigned long int tic, toc;
+    for (int i = 0; i < nombre_iter; i++)
+    {
+        unsigned int ui;
+
+        tic = __rdtscp(&ui);
+        ma_fonction(mon_arg);
+        toc = __rdtscp(&ui);
+
+        tab[i] = toc - tic;
+    }
+    ////
+
+    // moyenne
+
+    unsigned long int moyenne;
+
+    for (int i = 0; i < nombre_iter; i++)
+    {
+        moyenne += tab[i];
+    }
+    moyenne = moyenne / nombre_iter;
+    ////
+
+    printf("time spent : %lu\n", moyenne);
+}
+
+void main()
+{
+    int n = 1000000;
+
+    print_timing(n, acces_seq, 5, 10);
+    print_timing(n, acces_alea, 5, 10);
+}
